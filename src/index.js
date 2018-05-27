b6388c00006d0d2c89d16f7b7c9a49db6c83c677
Vignesh Nadar <vigneshnadar@Vigneshs-MacBook-Pro.local>
2018-05-27 15:19:08 -0400

    adding a widget

--- src/index.js
+++ src/index.js
@@ -4,24 +4,48 @@ import {Provider, connect} from 'react-redux'
 import {createStore} from 'redux'
 
 
+const Widget =({widget}) => (
+    <li >{widget.text}</li>
+)
+
 let initialState ={
     widgets: [
         {id: 0, text: 'Widget 1'},
-        {id: 0, text: 'Widget 2'},
-        {id: 0, text: 'Widget 3'}
+        {id: 1, text: 'Widget 2'},
+        {id: 2, text: 'Widget 3'}
     ]
 }
 
 
-const WidgetList = ({widgets}) => (
+const WidgetList = ({widgets, dispatch}) => (
     <div>
         <h1>Widget List: ({widgets.length})</h1>
+        <ul>
+            {widgets.map(widget => (
+                <Widget widget={widget} key={widget.id}/>
+            ))}
+        </ul>
+        <button onClick={e => (
+            dispatch({type : 'ADD_WIDGET'})
+        )}>Add Widget</button>
     </div>
 )
 
 
-const widgetReducer = () => {
-    return initialState
+let idAutoIncrement = 3
+
+const widgetReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'ADD_WIDGET':
+            return {
+                widgets: [
+                    ...state.widgets,
+                    {id: idAutoIncrement++, text: 'new widget'}
+                ]
+            }
+        default:
+            return state
+    }
 }
 
 
